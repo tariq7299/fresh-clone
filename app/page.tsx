0be41f3655355e3b72078c3487b4ac0f5a3cc204
Tariq Sarhan <tr.sar77an78@gmail.com>
2024-12-26 19:10:11 +0200

    - added the initial strucutre

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import NavBar from "./ui/components/nav-bar";
+
+
+export default function Home() {
+  return (
+    <div className="">
+      <main className="">
+        <NavBar />
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+
+      </footer>
+    </div>
+  );
+}
