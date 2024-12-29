4f7b362d4b5840b2a4e218c74454237e1e77e483
Tariq Sarhan <tr.sar77an78@gmail.com>
2024-12-29 22:24:57 +0200

    Changed some styling in the hero section

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { Combobox } from "./ui/components/combo-box";
 import { Button } from "./ui/components/custom/button";
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
 import { Separator } from "./ui/components/separator";
+import barberShop1 from "@/../public/barber-shop-1.jpg";
 
 export default function Home() {
   return (
@@ -11,27 +12,28 @@ export default function Home() {
       <main className="">
         <NavBar />
 
-        <section className=" p-5  grid justify-center relative">
-          {/* <div className=" bg-center"> */}
+        <section className=" p-5 pb-20 md:pb-48 grid justify-center relative">
+
+          {/* To make nextJs understand that this image is a local image you need to import it (at the top of the file) */}
           <Image
-            src="/barber-shop-1.jpg"
+            src={barberShop1}
             alt="Picture of the author"
-            width={1920}
-            height={1200}
-            className="absolute top-0 left-0 z-0 brightness-50 blur-sm bg-cover"
-          ></Image>
+            // With local images you don't need to provide the width and height
+            // width={1920}
+            // height={1200}
+            fill={true}
+            className="brightness-50 blur-sm object-cover"
+          />
 
 
-          {/* </div> */}
-
-          <div className="max-w-xl mt-24">
-            <h1 className="text-4xl font-black text-start sm:text-center tracking-tight font-libre-bodoni scale-y-110 text-white">Schedule local salon and wellness appointments</h1>
+          <div className="max-w-xl lg:max-w-2xl mt-24 relative">
+            <h1 className="text-4xl md:text-5xl lg:text-7xl font-black text-start sm:text-center tracking-tight font-libre-bodoni scale-y-110 text-white">Schedule local salon and wellness appointments</h1>
 
             {/* Repace this compnent with "./ui/components/custom/combo-box" */}
             <div>
 
               {/* Write types */}
-              <div className="flex gap-2 p-2 border rounded-lg justify-between mt-8 items-center ">
+              <div className="flex gap-2 p-2 border rounded-lg justify-between mt-8 items-center bg-white  ">
 
                 <Combobox triggerClassName={"border-0 w-full z-10"} triggerIcon={null} labelClassName={"font-semibold"} popoverClassName={"w-[200px] sm:w-[400px]"} />
 
