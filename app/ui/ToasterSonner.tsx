dc55c368a3890e0ada4ebe8b1b361329f0ca4366
Tariq Sarhan <tr.sar77an78@gmail.com>
2025-01-12 16:36:02 +0200

    Made the position of Sonner toaster as static in the top-center of the whole app

--- app/ui/ToasterSonner.tsx
+++ app/ui/ToasterSonner.tsx
@@ -2,20 +2,23 @@
 
 import { Toaster } from 'sonner'
 // import { useTheme } from '../theme-provider'
-import { useMediaQuery } from '@uidotdev/usehooks'
+// import { useMediaQuery } from '@uidotdev/usehooks'
 
 export default function SonnerToaster() {
     // const { theme } = useTheme()
-    const isDesktop = useMediaQuery("(min-width: 768px)")
+    // const isDesktop = useMediaQuery("(min-width: 768px)")
 
     return (
         <>
-            {isDesktop ?
+
+            <Toaster closeButton richColors={true} position="top-center" />
+
+            {/* {isDesktop ?
                 // (<Toaster theme={theme} closeButton expand={true} richColors={true} />)
                 (<Toaster closeButton expand={true} richColors={true} />)
                 :
                 // (<Toaster theme={theme} closeButton richColors={true} position="top-center" />)}
-                (<Toaster closeButton richColors={true} position="top-center" />)}
+                (<Toaster closeButton richColors={true} position="top-center" />)} */}
         </>
     )
 }
