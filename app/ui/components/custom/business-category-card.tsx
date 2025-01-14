bc9ee38c6d4f156f0422d9d45eab7ccb5e691808
Tariq Sarhan <tr.sar77an78@gmail.com>
2025-01-14 14:23:02 +0200

    Fixed some styling issues in category cards

--- app/ui/components/custom/business-category-card.tsx
+++ app/ui/components/custom/business-category-card.tsx
@@ -6,13 +6,14 @@ import { Button } from "./button"
 // Supply the col-span-1 class
 export default function BusinessCategoryCard({ categoryName, categoryIconUrl = "/categories/hair.png", categoryId }: { categoryName: string, categoryIconUrl: string, categoryId: string }) {
 
-    return <label htmlFor={categoryId} className="col-span-1 relative cursor-pointer w-full active:scale-95 transition-transform duration-150">
+    return <label htmlFor={categoryId} className="col-span-1 relative cursor-pointer w-full active:scale-95 transition-transform duration-150 before:content-[''] before:absolute before:inset-0 before:w-full before:h-full before:border-2 before:border-accent before:rounded-lg">
         <input id={categoryId} type="radio" name="business-category" value={categoryId} className="peer appearance-none hidden" />
         <div className={cn(
             "flex flex-col justify-center gap-3 items-start rounded-lg p-5 transition-all duration-200 box-border",
-            "border-1 border-gray-200 hover:bg-accent/5 ",
-            "peer-checked:border-accent peer-checked:border-2 peer-checked:bg-accent/5 peer-checked:text-accent-600 "
+            "border-1 border-gray-200 bg-white hover:bg-accent/5 ",
+            "peer-checked:border-accent  peer-checked:bg-accent/5 peer-checked:text-accent-600 relative "
         )}>
+
             <Image
                 src={categoryIconUrl}
                 alt={categoryName}
@@ -26,7 +27,7 @@ export default function BusinessCategoryCard({ categoryName, categoryIconUrl = "
             )}>{categoryName}</h1>
         </div>
         <div className={cn(
-            "absolute top-2 right-2 bg-accent text-background rounded-lg p-0.5",
+            "absolute top-2 right-2 bg-accent text-background rounded-lg p-0.5 scale-75",
             "opacity-0 ", // Hidden by default
             "transition-all duration-200",
             "peer-checked:opacity-100 peer-checked:scale-100" // Show and scale up when checked
