2dd7f0d023ba3325e9c3fb572d91e87c39c5a415
tariq7299 <tr.sar77an78@gmail.com>
2025-01-23 19:55:18 +0200

    Created the scroll to service nav pill mechanism

--- app/business/booking/services-list/page.tsx
+++ app/business/booking/services-list/page.tsx
@@ -1,70 +1,30 @@
 "use client"
 
-import { useScrollspy } from "@/lib/hooks/use-scroll-spy";
 import { cn } from "@/lib/utils/utils";
 import { Button } from "@/ui/components/custom/button";
-import { ScrollArea, ScrollBar } from "@/ui/components/scroll-area"
-import { useState, useEffect, RefObject } from "react"
+import { RefObject } from "react"
 import ScrollSpy from 'react-scrollspy-navigation';
 import { Plus } from 'lucide-react';
-import { Separator } from "@/ui/components/separator";
 import Image from "next/image";
 import barberShop from "@/../public/barber-shop-2.jpg"
 import { useRef, createRef } from "react";
 
 export default function BookingPage() {
 
-
-
-
-
-
     const tabTitles = ["Featured", "Hair", "Beard", "Facial", "Massage", "Manicure", "Pedicure", "Waxing", "Threading", "Body Treatment", "Spa Package", "Makeup", "Tanning", "Color Treatment", "Hair Treatment"]
 
     const tabsRefs = useRef<RefObject<HTMLAnchorElement | null>[]>(Array.from({ length: tabTitles.length }, () => createRef()));
 
-    const [currentSelectedTabIndex, setCurrentTabIndex] = useState<null | number>(null)
-    const [currentActiveId, setCurrentActiveId] = useState<null | string>(null)
-    console.log("tabsRefs", tabsRefs)
-
-    useEffect(() => {
-
-
-        if (currentActiveId && tabsRefs.current) {
-
-            console.log("currentSelectedTabIndex", currentSelectedTabIndex)
-
-            // tabsRefs.current?.[currentSelectedTabIndex].current.scrollIntoView({ behavior: 'smooth', block: 'center' })
-            const element = tabsRefs.current.find(ref => {
-                console.log("ref", ref)
-                return ref.current?.innerText === currentActiveId
-            })?.current
-            console.log("element", element)
-            element?.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'start' })
-
-        }
-    }, [currentActiveId])
-
-    const onClickEach = (e, next, container) => {
-        // console.log('The clicked element:', e.target);
-        // e.target.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'start' })
-        // console.log('The target container element:', container);
-        // setCurrentTabIndex(index)
-        next();
-    };
-
-    const onChangeActiveId = (current, prev) => {
-        console.log('Active element changed');
-        // const element = tabsRefs.current.find(ref => {
-        //     console.log("ref", ref)
-        //     return ref.current?.innerText === current
-        // })?.current
-        // console.log("element", element)
-        // element?.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'start' })
-
-        setCurrentActiveId(current)
-        console.log({ current, prev });
-    };
+    const onChangeActiveId = (current: string, prev: string) => {
+        const element = tabsRefs.current.find(ref => ref.current?.innerText === current)?.current
+        const container = element?.parentElement;
+        const offsetLeft = element?.offsetLeft; // Element's position within the container
+        console.log("offsetLeft", offsetLeft)
+        container?.scrollTo({
+            left: offsetLeft,
+            behavior: "smooth",
+        });
+    }
 
     return <div className="w-full max-w-7xl p-5 py-24 min-h-dvh  mx-auto space-y-8 pb-20" >
         <div className=" grid grid-cols-1 md:grid-cols-3  gap-6">
@@ -75,17 +35,13 @@ export default function BookingPage() {
 
                 {/* Services categories tabs */}
                 <nav className="w-full bg-background sticky top-16 left-0">
-                    <ScrollSpy onClickEach={onClickEach} onChangeActiveId={onChangeActiveId} activeClass="nav-active" activeAttr={true} offsetTop={0} offsetLeft={0} >
-                        <ScrollArea className="w-full whitespace-nowrap relative  ">
-                            <div className="flex gap-2 py-4 w-full">
-                                {tabTitles.map((title, index) => (
-                                    <a href={`#${title}`} className={cn(" px-4 py-2 rounded-full font-bold  text-foreground", "data-[active=true]:bg-foreground data-[active=true]:text-background hover:bg-muted  ")} key={title} ref={tabsRefs.current[index]}>{title}</a>
-                                ))}
-                            </div>
-                            <div className="absolute bottom-0 right-0 w-20 h-full  bg-gradient-to-l from-background to-transparent"></div>
-                            <ScrollBar orientation="horizontal" />
-                        </ScrollArea>
-
+                    <ScrollSpy onChangeActiveId={onChangeActiveId} activeClass="nav-active" activeAttr={true} offsetTop={0} offsetLeft={0} >
+                        <div className="flex gap-2 py-3 w-full whitespace-nowrap relative overflow-x-auto">
+                            {tabTitles.map((title, index) => (
+                                <a href={`#${title}`} className={cn(" px-4 py-2 rounded-full font-bold  text-foreground", "data-[active=true]:bg-foreground data-[active=true]:text-background hover:bg-muted  ")} key={title} ref={tabsRefs.current[index]}>{title}</a>
+                            ))}
+                        </div>
+                        <div className="absolute bottom-0 right-0 w-20 h-full  bg-gradient-to-l from-background to-transparent"></div>
                     </ScrollSpy>
                 </nav>
 
@@ -189,8 +145,8 @@ export default function BookingPage() {
 
                     </div>
                     <div id="Hair" className="flex flex-col gap-4">
-                        {/* Featured */}
-                        <h2 className="text-2xl md:text-3xl font-bold font-source-sans pb-3">Featured</h2>
+                        {/* Hair */}
+                        <h2 className="text-2xl md:text-3xl font-bold font-source-sans pb-3">Hair</h2>
 
 
                         <div className="flex justify-between items-center w-full md:border border-gray-200 md:rounded-lg md:p-4 border-b pb-4" >
@@ -286,8 +242,8 @@ export default function BookingPage() {
 
                     </div>
                     <div id="Beard" className="flex flex-col gap-4">
-                        {/* Featured */}
-                        <h2 className="text-2xl md:text-3xl font-bold font-source-sans pb-3">Featured</h2>
+                        {/* Beard */}
+                        <h2 className="text-2xl md:text-3xl font-bold font-source-sans pb-3">Beard</h2>
 
 
                         <div className="flex justify-between items-center w-full md:border border-gray-200 md:rounded-lg md:p-4 border-b pb-4" >
@@ -383,8 +339,8 @@ export default function BookingPage() {
 
                     </div>
                     <div id="Facial" className="flex flex-col gap-4">
-                        {/* Featured */}
-                        <h2 className="text-2xl md:text-3xl font-bold font-source-sans pb-3">Featured</h2>
+                        {/* Facial */}
+                        <h2 className="text-2xl md:text-3xl font-bold font-source-sans pb-3">Facial</h2>
 
 
                         <div className="flex justify-between items-center w-full md:border border-gray-200 md:rounded-lg md:p-4 border-b pb-4" >
@@ -480,8 +436,8 @@ export default function BookingPage() {
 
                     </div>
                     <div id="Massage" className="flex flex-col gap-4">
-                        {/* Featured */}
-                        <h2 className="text-2xl md:text-3xl font-bold font-source-sans pb-3">Featured</h2>
+                        {/* Massage */}
+                        <h2 className="text-2xl md:text-3xl font-bold font-source-sans pb-3">Massage</h2>
 
 
                         <div className="flex justify-between items-center w-full md:border border-gray-200 md:rounded-lg md:p-4 border-b pb-4" >
