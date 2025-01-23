5e87afd2f0e7648320d351c3fea4a226ebf0d237
tariq7299 <tr.sar77an78@gmail.com>
2025-01-23 21:14:07 +0200

    :

--- app/business/booking/services-list/page.tsx
+++ app/business/booking/services-list/page.tsx
@@ -4,7 +4,7 @@ import { cn } from "@/lib/utils/utils";
 import { Button } from "@/ui/components/custom/button";
 import { RefObject } from "react"
 import ScrollSpy from 'react-scrollspy-navigation';
-import { Plus } from 'lucide-react';
+import { Plus, Check } from 'lucide-react';
 import Image from "next/image";
 import barberShop from "@/../public/barber-shop-2.jpg"
 import { useRef, createRef } from "react";
@@ -26,7 +26,7 @@ export default function BookingPage() {
         });
     }
 
-    return <div className="w-full max-w-7xl p-5 py-24 min-h-dvh  mx-auto space-y-8 pb-20" >
+    return <div className="w-full max-w-6xl p-5 py-24 min-h-dvh  mx-auto space-y-8 pb-20" >
         <div className=" grid grid-cols-1 md:grid-cols-3  gap-6">
 
             <div className="col-span-3 lg:col-span-2" >
@@ -52,7 +52,27 @@ export default function BookingPage() {
                         <h2 className="text-2xl md:text-3xl font-bold font-source-sans pb-3">Featured</h2>
 
 
-                        <div className="flex justify-between items-center w-full md:border border-gray-200 md:rounded-lg md:p-4 border-b pb-4" >
+                        <div className="flex justify-between items-center w-full md:border border-gray-200 md:rounded-lg md:p-5 border-b pb-4 ring-2 ring-accent bg-accent/5 text-accent-700 transition-colors duration-200 cursor-pointer hover:bg-accent/10" >
+                            <div>
+                                <p className="font-semibold text-lg">Featured</p>
+                                <p className="text-sm text-muted-foreground pb-3">10min</p>
+                                <p className="font-semibold text-sm">EGP 100</p>
+                            </div>
+                            <Button size={"icon"} className="font-semibold h-7 w-7 p-1  rounded-lg shadow-none text-background bg-accent transition-colors duration-200">
+                                <Check />
+                            </Button>
+                        </div>
+                        <div className="flex justify-between items-center w-full md:border border-gray-200 md:rounded-lg md:p-5 border-b pb-4 cursor-pointer hover:bg-accent/10 group" >
+                            <div>
+                                <p className="font-semibold text-lg">Featured</p>
+                                <p className="text-sm text-muted-foreground pb-3">10min</p>
+                                <p className="font-semibold text-sm">EGP 100</p>
+                            </div>
+                            <Button size={"icon"} className="font-semibold h-7 w-7 p-1 bg-muted rounded-lg shadow-none text-primary  group-hover:bg-accent/10">
+                                <Plus />
+                            </Button>
+                        </div>
+                        <div className="flex justify-between items-center w-full md:border border-gray-200 md:rounded-lg md:p-5 border-b pb-4" >
                             <div>
                                 <p className="font-semibold text-lg">Featured</p>
                                 <p className="text-sm text-muted-foreground pb-3">10min</p>
@@ -62,7 +82,7 @@ export default function BookingPage() {
                                 <Plus />
                             </Button>
                         </div>
-                        <div className="flex justify-between items-center w-full md:border border-gray-200 md:rounded-lg md:p-4 border-b pb-4" >
+                        <div className="flex justify-between items-center w-full md:border border-gray-200 md:rounded-lg md:p-5 border-b pb-4" >
                             <div>
                                 <p className="font-semibold text-lg">Featured</p>
                                 <p className="text-sm text-muted-foreground pb-3">10min</p>
@@ -72,7 +92,7 @@ export default function BookingPage() {
                                 <Plus />
                             </Button>
                         </div>
-                        <div className="flex justify-between items-center w-full md:border border-gray-200 md:rounded-lg md:p-4 border-b pb-4" >
+                        <div className="flex justify-between items-center w-full md:border border-gray-200 md:rounded-lg md:p-5 border-b pb-4" >
                             <div>
                                 <p className="font-semibold text-lg">Featured</p>
                                 <p className="text-sm text-muted-foreground pb-3">10min</p>
@@ -82,7 +102,7 @@ export default function BookingPage() {
                                 <Plus />
                             </Button>
                         </div>
-                        <div className="flex justify-between items-center w-full md:border border-gray-200 md:rounded-lg md:p-4 border-b pb-4" >
+                        <div className="flex justify-between items-center w-full md:border border-gray-200 md:rounded-lg md:p-5 border-b pb-4" >
                             <div>
                                 <p className="font-semibold text-lg">Featured</p>
                                 <p className="text-sm text-muted-foreground pb-3">10min</p>
@@ -92,7 +112,7 @@ export default function BookingPage() {
                                 <Plus />
                             </Button>
                         </div>
-                        <div className="flex justify-between items-center w-full md:border border-gray-200 md:rounded-lg md:p-4 border-b pb-4" >
+                        <div className="flex justify-between items-center w-full md:border border-gray-200 md:rounded-lg md:p-5 border-b pb-4" >
                             <div>
                                 <p className="font-semibold text-lg">Featured</p>
                                 <p className="text-sm text-muted-foreground pb-3">10min</p>
@@ -102,7 +122,7 @@ export default function BookingPage() {
                                 <Plus />
                             </Button>
                         </div>
-                        <div className="flex justify-between items-center w-full md:border border-gray-200 md:rounded-lg md:p-4 border-b pb-4" >
+                        <div className="flex justify-between items-center w-full md:border border-gray-200 md:rounded-lg md:p-5 border-b pb-4" >
                             <div>
                                 <p className="font-semibold text-lg">Featured</p>
                                 <p className="text-sm text-muted-foreground pb-3">10min</p>
@@ -112,7 +132,7 @@ export default function BookingPage() {
                                 <Plus />
                             </Button>
                         </div>
-                        <div className="flex justify-between items-center w-full md:border border-gray-200 md:rounded-lg md:p-4 border-b pb-4" >
+                        <div className="flex justify-between items-center w-full md:border border-gray-200 md:rounded-lg md:p-5 border-b pb-4" >
                             <div>
                                 <p className="font-semibold text-lg">Featured</p>
                                 <p className="text-sm text-muted-foreground pb-3">10min</p>
@@ -122,26 +142,7 @@ export default function BookingPage() {
                                 <Plus />
                             </Button>
                         </div>
-                        <div className="flex justify-between items-center w-full md:border border-gray-200 md:rounded-lg md:p-4 border-b pb-4" >
-                            <div>
-                                <p className="font-semibold text-lg">Featured</p>
-                                <p className="text-sm text-muted-foreground pb-3">10min</p>
-                                <p className="font-semibold text-sm">EGP 100</p>
-                            </div>
-                            <Button size={"icon"} className="font-semibold h-7 w-7 p-1 bg-muted rounded-lg shadow-none text-primary">
-                                <Plus />
-                            </Button>
-                        </div>
-                        <div className="flex justify-between items-center w-full md:border border-gray-200 md:rounded-lg md:p-4 border-b pb-4" >
-                            <div>
-                                <p className="font-semibold text-lg">Featured</p>
-                                <p className="text-sm text-muted-foreground pb-3">10min</p>
-                                <p className="font-semibold text-sm">EGP 100</p>
-                            </div>
-                            <Button size={"icon"} className="font-semibold h-7 w-7 p-1 bg-muted rounded-lg shadow-none text-primary">
-                                <Plus />
-                            </Button>
-                        </div>
+
 
                     </div>
                     <div id="Hair" className="flex flex-col gap-4">
@@ -544,7 +545,7 @@ export default function BookingPage() {
             </div>
 
             <div className="col-span-1  relative hidden lg:block">
-                <div className="sticky top-16 left-0 p-5 ">
+                <div className="sticky top-16 left-0 pt-5 ">
 
                     <div className="border border-gray-200 rounded-lg p-5 space-y-4">
 
