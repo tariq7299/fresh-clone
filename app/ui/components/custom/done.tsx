3f4828591623c2ae9138510065b27ba47d10f0f6
Tariq Sarhan <tr.sar77an78@gmail.com>
2025-01-26 20:45:17 +0200

    Styled the resercvation confirmation page

--- app/ui/components/custom/done.tsx
+++ app/ui/components/custom/done.tsx
@@ -1,41 +1,54 @@
 import { Button } from "@/ui/components/custom/button"
 import Link from "next/link"
-import { CircleCheckBig } from 'lucide-react';
+import { CircleCheck } from 'lucide-react';
 
 export default function Done() {
-    return <div className="flex flex-col items-center justify-start h-screen w-full py-10">
+    return <div className="flex flex-col items-start justify-start h-screen  py-24 ">
 
-        <h1 className="text-4xl font-bold font-lora">Done And Done!</h1>
+        <div className="mx-auto space-y-4 max-w-screen-sm w-full">
 
-        <div className="flex justify-start items-center gap-4">
+            <h1 className="text-6xl font-bold font-lora text-start">Done And Done!</h1>
 
-            <div className="flex-none">
-                <CircleCheckBig className="size-10 text-success-600" />
+            <div className="flex flex-col gap-4">
+
+
+                <div className="flex justify-start items-center gap-4  bg-success-100 p-4 rounded-md">
+
+                    <div className="flex-none">
+                        <CircleCheck className="size-8 text-primary" />
+                    </div>
+
+                    <div className="flex flex-col space-y-2 rounded-lg grow">
+                        <p className="text-xl font-bold font-source-sans text-primary">Success</p>
+                        <p className="text- ">Your appointment has been scheduled successfully.</p>
+                        <p className="text- ">We look forward to seeing you!</p>
+                    </div>
+
+                </div>
+
+                <div className="">
+                    <p className="text-lg font-medium">
+                        We've sent a confirmation email to <span className="font-semibold">user@example.com</span> with all the details of your appointment.
+                    </p>
+                    <p className="text-sm">Please check your inbox (and spam folder) for the confirmation.</p>
+                </div>
+
+
             </div>
-            <div className="flex flex-col gap-2 bg-success-400 p-4 rounded-lg grow">
-                <p>Success</p>
-                <p className="text-lg font-medium">Your appointment has been scheduled successfully.</p>
-                <p className="text-lg font-medium">We look forward to seeing you!</p>
-            </div>
-            <div className="flex flex-col gap-2 bg-success-400 p-4 rounded-lg grow mt-4">
-                <p className="text-lg font-medium">
-                    We've sent a confirmation email to <span className="font-semibold">user@example.com</span> with all the details of your appointment.
-                </p>
-                <p>Please check your inbox (and spam folder) for the confirmation.</p>
-            </div>
-        </div>
 
-        <div className="flex gap-4">
-            <Button>
-                {/* <Link href="/"> */}
-                Go to Home
-                {/* </Link> */}
-            </Button>
-            <Button>
-                {/* <Link href="/business/reservations"> */}
-                Go to Reservations
-                {/* </Link> */}
-            </Button>
+
+            <div className="flex gap-4">
+                <Button variant={"outline"}>
+                    {/* <Link href="/"> */}
+                    Go to Home
+                    {/* </Link> */}
+                </Button>
+                <Button>
+                    {/* <Link href="/business/reservations"> */}
+                    Go to Reservations
+                    {/* </Link> */}
+                </Button>
+            </div>
         </div>
 
     </div>
